a38e9bc27fc43dcf4c6909afad1ce220468f427d
Pedro Barison <pedrobarison@gmail.com>
2026-05-19 20:45:26 -0300

    Enviando arquivos iniciais do projeto Django

--- a3_backendapp/settings.py
+++ a3_backendapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production.
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
