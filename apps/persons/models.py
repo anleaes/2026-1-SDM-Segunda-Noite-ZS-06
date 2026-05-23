dc9296fa45a756bc34105786598697458d93a1d0
Pedro Barison <pedrobarison@gmail.com>
2026-05-22 23:11:44 -0300

    Criação dos atributos em Models

--- apps/persons/models.py
+++ apps/persons/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField('Nome', max_length=50)
+    last_name = models.CharField('Sobrenome', max_length=100)
+    birth_date = models.DateField('Data de Nascimento', null=True, blank=True)
+    email = models.EmailField('E-mail',null=False, blank=False)
