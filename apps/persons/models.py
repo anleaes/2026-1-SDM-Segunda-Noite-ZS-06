75ebd7cbbc15bc523b84a14d9db70307d1d04bd7
Pedro Barison <pedrobarison@gmail.com>
2026-05-22 23:12:20 -0300

    Criação do nome no Models

--- apps/persons/models.py
+++ apps/persons/models.py
@@ -6,3 +6,11 @@ class Person(models.Model):
     last_name = models.CharField('Sobrenome', max_length=100)
     birth_date = models.DateField('Data de Nascimento', null=True, blank=True)
     email = models.EmailField('E-mail',null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+        ordering =['id']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
