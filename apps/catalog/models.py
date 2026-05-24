e64e80c6eab7eedf25f1f4b708b7a410cbadded2
DVk004 <glazepurplix@gmail.com>
2026-05-23 23:32:36 -0300

    chore: gera migrations pra tabela de developer

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Developer(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    foundation_year = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
