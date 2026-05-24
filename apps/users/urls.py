f1e2983ac303c2bba8b2a5c73dbdc0f651c63ff4
Pedro Barison <pedrobarison@gmail.com>
2026-05-23 23:30:56 -0300

    Configuração do Urls de Users

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+app_name = 'users'
+
+router = routers.SimpleRouter()
+router.register('', views.UserViewSet, basename='usuarios')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
