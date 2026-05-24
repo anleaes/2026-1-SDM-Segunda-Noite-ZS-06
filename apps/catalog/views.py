9ead4b8f0d8cc6cae783906c6e1faa8baea917c3
DVk004 <glazepurplix@gmail.com>
2026-05-24 20:47:12 -0300

    feat: implementada ConsoleViewSet completa

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -5,6 +5,15 @@ from .serializer import DeveloperSerializer, GenreSerializer, ConsoleSerializer
 class DeveloperViewSet(viewsets.ModelViewSet):
     queryset = Developer.objects.all()
     serializer_class = DeveloperSerializer
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class ConsoleViewSet(viewsets.ModelViewSet):
+    queryset = Console.objects.all()
+    serializer_class = ConsoleSerializer
+
 from django.shortcuts import render
 
 # Create your views here.
