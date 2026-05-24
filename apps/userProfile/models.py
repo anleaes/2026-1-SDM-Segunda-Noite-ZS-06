f94c9babdcd4a874bfd148a149985c3682278d3d
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 19:23:42 -0300

    Criação dos atributos no Model do userProfile

--- apps/userProfile/models.py
+++ apps/userProfile/models.py
@@ -2,3 +2,11 @@ from django.db import models
 from users.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    avatar = models.ImageField('Avatar', upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField('Biografia', max_length=2000, blank=True)
+    country = models.CharField('País', max_length=100)
+    games_added = models.IntegerField('Jogos Adicionados', default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+   
