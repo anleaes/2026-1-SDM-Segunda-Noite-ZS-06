031d8c24803e5f2b2f689c77c44698819696f8f6
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 16:26:33 -0300

    Configuração do Views de Admins

--- apps/admins/views.py
+++ apps/admins/views.py
@@ -5,3 +5,6 @@ from .serializer import AdminSerializer
 
 
 # Create your views here.
+class AdminViewSet(viewsets.ModelViewSet):
+    queryset = Admin.objects.all()
+    serializer_class = AdminSerializer  
