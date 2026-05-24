2584cbaa975cbb09c713be1f17e33ebd766af06a
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 19:45:33 -0300

    Implementação do Views do userProfile

--- apps/userProfile/views.py
+++ apps/userProfile/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import User
+from .models import UserProfile
 from rest_framework import viewsets
 from .serializer import UserProfileSerializer
 
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer  
 
