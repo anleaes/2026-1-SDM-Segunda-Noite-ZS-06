e46c3e772156284a77c541891eda60228aad5b21
DVk004 <glazepurplix@gmail.com>
2026-05-24 20:49:09 -0300

    feat: registra a rota console no urls.py do catalog

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -5,7 +5,10 @@ from . import views
 app_name = 'catalog'
 
 router = DefaultRouter()
+# Rotas registradas:
 router.register(r'developer', views.DeveloperViewSet, basename='developer')
+router.register(r'genre', views.GenreViewSet, basename='genre')
+router.register(r'console', views.ConsoleViewSet, basename='console')
 
 urlpatterns = [
     path('', include(router.urls)),
