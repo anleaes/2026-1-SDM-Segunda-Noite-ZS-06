8056ff6d48421c590c957694f04d5a5fb9b38c1e
DVk004 <glazepurplix@gmail.com>
2026-05-24 20:39:59 -0300

    feat: importa o model Console no serializer

--- apps/catalog/serializer.py
+++ apps/catalog/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Developer
+from .models import Developer, Genre, Console
 
 class DeveloperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Developer
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
         fields = '__all__'
