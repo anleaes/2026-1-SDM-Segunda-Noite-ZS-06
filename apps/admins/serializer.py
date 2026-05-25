5532ba2afc8da28153f6f772375bbe5a7abb2e4e
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 22:20:11 -0300

    Adição de tratamento na senha em usuarios e administradores

--- apps/admins/serializer.py
+++ apps/admins/serializer.py
@@ -5,3 +5,17 @@ class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admin
         fields = ['first_name', 'last_name', 'birth_date', 'email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Remove a senha dos dados brutos
+        password = validated_data.pop('senha', None)
+        # Cria o objeto do usuário sem a senha primeiro
+        instance = self.Meta.model(**validated_data)
+        
+        if password is not None:
+            # Encriptação da senha
+            instance.set_password(password)
+        
+        instance.save()
+        return instance
