028274f3488acb359b5a8d71d82222b3fe672bcf
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 21:54:24 -0300

    Adição de autenticação de classe e permissão da cçasse no views.py de admin e user

--- apps/admins/views.py
+++ apps/admins/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from .models import Admin
 from rest_framework import viewsets, permissions
 from .serializer import AdminSerializer
+from rest_framework.permissions import AllowAny
 
 
 # Create your views here.
 class AdminViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
     queryset = Admin.objects.all()
     serializer_class = AdminSerializer  
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from .models import User
 from rest_framework import viewsets, permissions
 from .serializer import UserSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
     queryset = User.objects.all()
     serializer_class = UserSerializer  
 
