1ab7742f5e6c281d5761c5f9786b3c9d5e34d219
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 21:35:26 -0300

    Adição de permissões pra criar conta no views.py do users

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializer import UserSerializer
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer  
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.AllowAny()] # Libera pra criar a conta
+        return [permissions.IsAuthenticated()] # Bloqueia o resto, a ideia é que sem uma conta, tu tenha apenas a aba de criação de conta liberada
