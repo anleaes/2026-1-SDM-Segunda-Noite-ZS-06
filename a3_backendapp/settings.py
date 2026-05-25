a327cea4710a155caec08b2b7b285e980fa318e1
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 21:43:36 -0300

    Permissão definida pela view. Mudança no REST_FRAMEWORK no settings.py

--- a3_backendapp/settings.py
+++ a3_backendapp/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {              #Configuração pra ninguém entrar sem estar logado, tem que logar antes, não mexe nisso, ainda to arrumando
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny', 
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
