37e310b19bbc85a438041d6afcf72f9b0d47cbe9
Pedro Barison <pedrobarison@gmail.com>
2026-05-24 21:29:14 -0300

    Configuração do views.py do userProfiles pra login de usuario

--- apps/userProfile/views.py
+++ apps/userProfile/views.py
@@ -5,6 +5,13 @@ from .serializer import UserProfileSerializer
 
 # Create your views here.
 class UserProfileViewSet(viewsets.ModelViewSet):
-    queryset = UserProfile.objects.all()
+    #apaguei o queryset, preciso colocar outras informações pra manter a função de login funcional
     serializer_class = UserProfileSerializer  
-
+    
+    def get_queryset(self):
+        # Essa linha faz com que o usuário veja só o perfil dele, acho que tá funcionando
+        return UserProfile.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        # Isso daqui salva o perfil pra logar automaticamente, acho que funcionda
+        serializer.save(user=self.request.user)
