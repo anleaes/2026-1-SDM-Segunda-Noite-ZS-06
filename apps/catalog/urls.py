007c9f83f7ff629f2147d2ecdbb4c7527fddf612
DVk004 <glazepurplix@gmail.com>
2026-05-28 22:13:18 -0300

    implementada funcionalidade completa

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -6,10 +6,11 @@ app_name = 'catalog'
 
 router = DefaultRouter()
 # Rotas registradas:
+router.register(r'game', views.GameViewSet, basename='game')
 router.register(r'developer', views.DeveloperViewSet, basename='developer')
 router.register(r'genre', views.GenreViewSet, basename='genre')
 router.register(r'console', views.ConsoleViewSet, basename='console')
-router.register(r'game', views.GameViewSet, basename='game')
+router.register(r'screenshot', views.ScreenshotViewSet, basename='screenshot')
 
 urlpatterns = [
     path('', include(router.urls)),
