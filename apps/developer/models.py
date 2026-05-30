052ca41884c3916405b74232e506e4a49829a18b
DVk004 <glazepurplix@gmail.com>
2026-05-30 17:10:39 -0300

    feito o models do Developer agora separado

--- apps/developer/models.py
+++ apps/developer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Developer(models.Model):
+    name = models.CharField(max_length=150)
+    country = models.CharField(max_length=100)
+    foundation_year = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'catalog_developer'
+
+    def __str__(self):
+        return self.name
