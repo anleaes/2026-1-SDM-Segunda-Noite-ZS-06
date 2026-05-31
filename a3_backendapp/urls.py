63d410f3dfb8f40e90587357209fd9d9f7bb37db
Pedro Barison <pedrobarison@gmail.com>
2026-05-31 03:06:24 -0300

    Adições no urls.py principal, pra conexão com React

--- a3_backendapp/urls.py
+++ a3_backendapp/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from users.views import logout_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,3 +36,9 @@ urlpatterns = [
     path('administradores/', include('admins.urls', namespace='admins')),
     path('perfis de usuarios/', include('userProfile.urls', namespace='userProfile')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
