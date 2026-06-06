daf68c4ff1ddde00cd43a5088784125a9374e653
Pedro Barison <pedrobarison@gmail.com>
2026-06-06 19:53:13 -0300

    Melhora da view do userProfile

--- apps/userProfile/serializer.py
+++ apps/userProfile/serializer.py
@@ -1,7 +1,10 @@
 from .models import User, UserProfile
 from rest_framework import serializers
+from users.models import User
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+
     class Meta:
         model = UserProfile
-        fields = ['id', 'avatar', 'bio', 'country', 'games_added'] # ao invés de '__all__', estou configurando isso, não mexe
+        fields = ['id', 'avatar', 'bio', 'country', 'games_added', 'username'] # ao invés de '__all__', estou configurando isso, não mexe
