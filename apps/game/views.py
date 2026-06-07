ce83671e96c3ff0d199e5a3a0dab82fba2e8af8e
Pedro Barison <pedrobarison@gmail.com>
2026-06-07 19:55:55 -0300

    Funcionalidade de adicionar e excluir do carrossel por usuario

--- apps/game/views.py
+++ apps/game/views.py
@@ -19,3 +19,21 @@ class ScreenshotViewSet(viewsets.ModelViewSet):
             
         # A CORREÇÃO: Garante que a lista volte ordenada da foto mais antiga para a mais nova
         return queryset.order_by('upload_date')
+    
+
+    def perform_create(self, serializer):
+        jogo = serializer.validated_data['game']
+        
+        # Verifica se o ID do usuário que mandou a requisição é igual ao ID do criador do jogo
+        if jogo.added_by != self.request.user:
+            raise PermissionDenied("Acesso negado: Você só pode adicionar fotos aos jogos que você mesmo cadastrou.")
+        
+        serializer.save()
+
+    # 2. O SEGURANÇA DA EXCLUSÃO:
+    def perform_destroy(self, instance):
+        # O 'instance' é a foto que está tentando ser apagada. Olhamos para o dono do jogo dela.
+        if instance.game.added_by != self.request.user:
+            raise PermissionDenied("Acesso negado: Você só pode apagar fotos dos jogos que você mesmo cadastrou.")
+        
+        instance.delete()
