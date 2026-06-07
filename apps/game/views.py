cbad9ea78d86b1cede4680bc16b766236fcc450a
Pedro Barison <pedrobarison@gmail.com>
2026-06-07 03:13:57 -0300

    ajuste no views de game pro screenshot

--- apps/game/views.py
+++ apps/game/views.py
@@ -7,5 +7,12 @@ class GameViewSet(viewsets.ModelViewSet):
     serializer_class = GameSerializer
 
 class ScreenshotViewSet(viewsets.ModelViewSet):
-    queryset = Screenshot.objects.all()
     serializer_class = ScreenshotSerializer
+
+    def get_queryset(self):
+        queryset = Screenshot.objects.all()
+        # Permite que o app peça as fotos apenas do jogo atual (/screenshot/?game=5)
+        game_id = self.request.query_params.get('game', None)
+        if game_id is not None:
+            queryset = queryset.filter(game_id=game_id)
+        return queryset
