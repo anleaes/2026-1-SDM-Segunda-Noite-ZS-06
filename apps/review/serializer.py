a0f2ef36c8d93eb1de78fd7b59639d5dac3c0b62
Pedro Barison <pedrobarison@gmail.com>
2026-06-07 00:29:14 -0300

    Puxei o nome de usuario no serializer do Review

--- apps/review/serializer.py
+++ apps/review/serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+
     class Meta:
         model = Review
         fields = '__all__'
