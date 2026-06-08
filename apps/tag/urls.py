169cf1e9f4730f5d86ee3a18ce281499d260afbd
Pedro Barison <pedrobarison@gmail.com>
2026-06-08 01:58:52 -0300

    mudança da rota da tag

--- apps/tag/urls.py
+++ apps/tag/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 from . import views
 
 app_name = 'tag'
 router = routers.SimpleRouter()
-router.register('', views.TagViewSet, basename='tag')
+
+router.register(r'categoria', views.TagViewSet, basename='tag')
 router.register(r'gametag', views.GameTagViewSet, basename='gametag')
 
 urlpatterns = [
