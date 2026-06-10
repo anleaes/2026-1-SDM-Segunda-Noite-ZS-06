d8b943f08f40a44263ad4f4c0da73622ec91fe90
Pedro Barison <pedrobarison@gmail.com>
2026-06-10 04:20:22 -0300

    Ajustes pra permissão por Vue

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -5,10 +5,10 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'birth_date', 'email', 'username', 'password']
+        # 'is_superuser' aqui também
+        fields = ['first_name', 'last_name', 'birth_date', 'email', 'username', 'password', 'is_superuser']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Criptografa a senha antes de salvar
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
