837f274393046d23f7c017150108aba934198694
DVk004 <glazepurplix@gmail.com>
2026-06-12 02:34:01 -0300

    feat: implementadas views tradicionais para CRUD de jogos

--- apps/game/views.py
+++ apps/game/views.py
@@ -44,3 +44,55 @@ class ScreenshotViewSet(viewsets.ModelViewSet):
             raise PermissionDenied("Acesso negado: Você só pode apagar fotos dos jogos que você mesmo cadastrou.")
         
         instance.delete()
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import GameForm
+from .models import Game
+
+@login_required(login_url='/contas/login/')
+def list_games_view(request):
+    query = request.GET.get('query', '')
+    if query:
+        games = Game.objects.filter(title__icontains=query)
+    else:
+        games = Game.objects.all()
+    return render(request, 'game/list_games.html', {'games': games, 'query': query})
+
+@login_required(login_url='/contas/login/')
+def add_game_view(request):
+    if request.method == 'POST':
+        form = GameForm(request.POST, request.FILES)
+        if form.is_valid():
+            game = form.save(commit=False)
+            game.added_by = request.user
+            game.save()
+            form.save_m2m()  # Necessário para salvar relacionamentos ManyToMany (genres/consoles)
+            return redirect('game:list_games_view')
+    else:
+        form = GameForm()
+    return render(request, 'game/add_game.html', {'form': form, 'action': 'Adicionar'})
+
+@login_required(login_url='/contas/login/')
+def edit_game_view(request, id_game):
+    game = get_object_or_404(Game, id=id_game)
+    # Restrição de edição para dono do cadastro ou equipe técnica
+    if game.added_by != request.user and not request.user.is_staff:
+        return render(request, '403.html', {'message': 'Você não tem permissão para editar este jogo.'}, status=403)
+    
+    if request.method == 'POST':
+        form = GameForm(request.POST, request.FILES, instance=game)
+        if form.is_valid():
+            form.save()
+            return redirect('game:list_games_view')
+    else:
+        form = GameForm(instance=game)
+    return render(request, 'game/add_game.html', {'form': form, 'action': 'Editar'})
+
+@login_required(login_url='/contas/login/')
+def delete_game_view(request, id_game):
+    game = get_object_or_404(Game, id=id_game)
+    if game.added_by != request.user and not request.user.is_staff:
+        return render(request, '403.html', {'message': 'Você não tem permissão para excluir este jogo.'}, status=403)
+    game.delete()
+    return redirect('game:list_games_view')
